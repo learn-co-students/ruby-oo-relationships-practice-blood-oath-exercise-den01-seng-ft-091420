96e1e10be492a28414109c2c97aaeb733e341e61
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-10-30 13:13:37 -0600

    updated seed typos

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,8 @@ def reload
   load 'config/environment.rb'
 end
 
+binding.pry
+
 # cult seeds 
 cromulons = Cult.new('Cromulons', 'Cygnus-5', 2015, 'Get Schwifty')
 jonestown = Cult.new('Jonestown', 'Indianapolis', 1950, 'We drink the koolaid')
@@ -22,19 +24,14 @@ ed = Follower.new('Ed', 28, 'Read books')
 
 # bloodoath seeds
 oath1 = Bloodoath.new(cromulons, summer, '2015/2/11')
-oath2 = Bloodoath.new(joneston, jim, '1951/1/1')
-oath3 = Bloodoath.new(joneston, bonnie, '1951/1/1')
-oath4 = Bloodoath.new(joneston, joseph, '1952/1/1')
-oath5 = Bloodoath.new(joneston, charles, '1952/1/1')
+oath2 = Bloodoath.new(jonestown, jim, '1951/1/1')
+oath3 = Bloodoath.new(jonestown, bonnie, '1951/1/1')
+oath4 = Bloodoath.new(jonestown, joseph, '1952/1/1')
+oath5 = Bloodoath.new(jonestown, charles, '1952/1/1')
 oath6 = Bloodoath.new(heavensgate, bonnie, '1974/1/1')
-oath7 = Bloodoath.new(heavensgate, adam, '1988/11/1')
+oath7 = Bloodoath.new(heavensgate, ed, '1988/11/1')
 oath8 = Bloodoath.new(solartemple, joseph, '1989/12/1')
 oath8 = Bloodoath.new(solartemple, ted, '1990/4/1')
 oath8 = Bloodoath.new(solartemple, ed, '1994/3/2')
-
-
-
-
-binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
